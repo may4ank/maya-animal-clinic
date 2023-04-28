6220b6c56010aba2ad0ae7a734df21952cdd4e40
mayank <mayanks561@gmail.com>
2023-04-28 09:39:09 -0400

    Adding Pojos
    Closes #2

--- src/main/java/com/example/mayaanimalclinic/repositories/AuthorRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.mayaanimalclinic.repositories;
-
-import com.example.mayaanimalclinic.Domain.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepo extends CrudRepository<Author, Long> {
-}
--- src/main/java/com/example/mayaanimalclinic/repositories/PublisherRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.mayaanimalclinic.repositories;
-
-import com.example.mayaanimalclinic.Domain.Publisher;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PublisherRepo extends CrudRepository<Publisher, Long> {
-}
